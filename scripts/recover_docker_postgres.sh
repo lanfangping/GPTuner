3662ddacbb7ec6b9bc4a33e5f61a56b87a7d5e9b
Fangping Lan <34638594+lanfangping@users.noreply.github.com>
2025-04-30 16:34:59 -0400

    foo

--- scripts/recover_docker_postgres.sh
+++ scripts/recover_docker_postgres.sh
@@ -2,5 +2,5 @@ sudo cp temp_pg/postgresql.conf ./pgdata/postgresql.conf
 sudo cp temp_pg/postgresql.auto.conf ./pgdata/postgresql.auto.conf
 sudo cp temp_pg/pg_hba.conf ./pgdata/pg_hba.conf
 
-sudo docker restart tpcc_workload
+echo 251314 | sudo -S docker restart benchbase
 sleep 2
